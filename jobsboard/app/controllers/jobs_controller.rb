0929f2821497c9dce7438e8a9eef41eb6076d2ed
Mireille Bonapart <mireilleb1202@gmail.com>
2016-01-12 17:52:39 +0100

    Add method create new job

--- jobsboard/app/controllers/jobs_controller.rb
+++ jobsboard/app/controllers/jobs_controller.rb
@@ -1,9 +1,84 @@
 class JobsController < ApplicationController
-  def create
-  render :index
-  end
+ before_action :find_job, only: [:show, :edit, :update, :destroy]
 
-  def new
-  	@job = Job.new
-  end
-end
+ def index
+ end
+ 
+ def show
+ end	
+
+ def new
+ 	@job = Job.new
+ end
+ 
+ def create
+ 	@job = Job.new(job_params)	
+
+ 	if @job.save
+ 		redirect_to @job
+ 	else
+ 	    render "New"
+ 	end    	
+ end
+
+ def edit
+ end
+ 
+ def update
+ end
+
+ def destroy
+ end
+
+ private
+
+ def job_params
+   params.require(:job).permit(:title, :description, :company, :url)	
+ end
+
+ def find_job
+  @job=Job.find(params[:id]) 
+ end 
+
+end 
+
+
+	 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
